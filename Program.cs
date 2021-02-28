2f2c546368e57c91ea8aa3cf0feb1e91bb972e1c
juhriber <j.de.s.r.25@gmail.com>
2021-02-28 16:36:32 -0300

    Finalização do exercício ContagemRgressiva

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Preparar...");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Beep();
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Apontar...");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Beep();
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("FOGO!");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.Beep();
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;         
+            Console.WriteLine("Preparar...");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Beep();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Apontar...");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Beep();
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("FOGO!");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White; 
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ContagemRegressiva
-Faça um programa que exiba uma contagem regressiva no seguinte formato:  
+Faça um programa que exiba uma contagem regressiva no seguinte formato:
 
-Preparar... Apontar... FOGO! 
+Preparar... Apontar... FOGO!
 
 Aguarde o pressionamento de uma tecla antes de cada transição.
